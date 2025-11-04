8edb9aa49e592ce7dba757aea9c1dbbe8df9a9a3
aurorelp <aleperff@gmail.com>
2025-11-04 16:22:55 +0100

    solving css issue with render

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../src/session.php';
   <title>CheckIt</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+  <link rel="stylesheet" href="assets/css/override-bootstrap.css?v=2.1">
   <!-- Bootstrap JS (inclut Popper.js) -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 </head>
